8e958d682865447801f9d5f2694d0ca9bbdfab32
Matt Rich <metropolis_4@hotmail.com>
2015-06-26 10:29:58 -0600

    added pet owner class

--- main.js
+++ main.js
@@ -24,4 +24,19 @@
     this.color = color;
   };
 
+  var PetOwner = function(name, age, petSpecies, petColor, petName) {
+    this.name = name;
+    this.age = age;
+    Pet = function(species, color, name) {
+      this.species = species;
+      this.color = color;
+      this.name = name;
+    };
+    this.pet = new Pet(petSpecies, petColor, petName);
+  };
+  PetOwner.tellAboutPet = function () {
+    return "My pet's name is " + this.pet.name +
+    ". He is " + this.pet.color + " and his name is " + this.pet.name;
+  };
+
 }).call(this);
